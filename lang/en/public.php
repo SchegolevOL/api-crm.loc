91d373f2a1ce359ed5fcb4001a2901628c70f441
Oleg Schegol <olschegolev@yahoo.com>
2024-11-04 21:57:12 +0500

    add login form

---
+++ lang/en/public.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'login_title'=>'Login to your account',
+    'spanLogin'=>'Enter your credentials below',
+    'buttonLogin'=>'Login',
+    'enterEmail'=>'E-Mail Address',
+    'password'=>'Password',
+
+];
